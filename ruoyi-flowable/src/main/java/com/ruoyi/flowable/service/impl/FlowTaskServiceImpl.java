086ad69f235e1db66c0bab56d204c932fefc5a37
zhanqh <2855942332@qq.com>
2023-10-11 09:23:26 +0800

    流程多实例任务优化和接口添加swagger注解

--- ruoyi-flowable/src/main/java/com/ruoyi/flowable/service/impl/FlowTaskServiceImpl.java
+++ ruoyi-flowable/src/main/java/com/ruoyi/flowable/service/impl/FlowTaskServiceImpl.java
@@ -105,7 +105,7 @@ public class FlowTaskServiceImpl extends FlowServiceFactory implements IFlowTask
 
             taskService.complete(taskVo.getTaskId(), taskVo.getVariables());
 
-            /************************ 给多实例任务手动指定处理人 ***************************/
+            //*********************** 给多实例任务手动指定处理人 ***************************/
             // 查询流程定义
             List<UserTask> nextUserTasks = FindNextNodeUtil.getNextUserTasks(repositoryService, task);
             // 循环下一个节点的 UserTask 数据
@@ -122,7 +122,7 @@ public class FlowTaskServiceImpl extends FlowServiceFactory implements IFlowTask
                         List<Task> list = taskService.createTaskQuery()
                                 .processInstanceId(task.getProcessInstanceId())
                                 .taskDefinitionKeyLike("%" + n.getId() + "%").list();
-                        for (int i = 0; i < multiInstanceVar.size(); i++) {
+                        for (int i = 0; i < list.size(); i++) {
                             Task tempTask = list.get(i);
                             taskService.setAssignee(tempTask.getId(), String.valueOf(multiInstanceVar.get(i)));
                         }
